63c81fec5a168427fe45873f1696df6036e9ae05
Mohit Bisht <100072596+bishtmohit872@users.noreply.github.com>
2025-09-20 22:15:33 +0530

    Update page.jsx

--- app/(main)/expenses/new/page.jsx
+++ app/(main)/expenses/new/page.jsx
@@ -23,13 +23,13 @@ const NewExpensepage = () => {
 
             <Card>
                 <CardContent>
-                    <Tabs defaultValue="indiviual" className="pb-3">
+                    <Tabs defaultValue="individual" className="pb-3">
                         <TabsList className="grid w-full grid-cols-2">
-                            <TabsTrigger value="indiviual">Indiviual Expense</TabsTrigger>
+                            <TabsTrigger value="individual">Individual Expense</TabsTrigger>
                             <TabsTrigger value="group">Group Expense</TabsTrigger>
                         </TabsList>
-                        <TabsContent value="indiviual" className="mt-0">
-                            <ExpenseForm type="indiviual" onSuccess={(id)=>router.push(`/person/${id}`)}/>
+                        <TabsContent value="individual" className="mt-0">
+                            <ExpenseForm type="individual" onSuccess={(id)=>router.push(`/person/${id}`)}/>
                         </TabsContent>
                         <TabsContent value="group" className="mt-0">
                             <ExpenseForm type="group" onSuccess={(id)=>router.push(`/groups/${id}`)}/>

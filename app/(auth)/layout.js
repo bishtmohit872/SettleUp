fe25cc49df531f2d5d2139308c2c3f6b7b0838b9
Mohit Bisht <100072596+bishtmohit872@users.noreply.github.com>
2025-09-21 11:44:14 +0530

    Update layout.js

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const AuthLayout = ({ children }) => {
-  return <div className="flex justify-center pt-40">
+  return <div className="flex flex-col lg:flex-row items-center justify-center pt-40 px-40" >
+    <img className="hidden lg:block size-140" src="/login_back.jpg" alt="login"/>
     {children}
   </div>;
 };
